622e34202472da90a6d5d6f351732052b037c81c
grant-create <grant.s.wilkie@gmail.com>
2021-08-18 16:38:19 -0700

    rps update

--- day4/rock_paper_scissors.py
+++ day4/rock_paper_scissors.py
@@ -1,19 +1,21 @@
 import random
 move = int(input("Which do you choose? 0 for Rock, 1 for Paper, 2 for Scissors "))
 
-rps = ["rock", "paper", "scissors"]
+rps = ["Rock", "Paper", "Scissors"]
 
 random_int = random.randint(0,2)
-print(f"computer chose: {rps[random_int]} ||",f"you chose: {rps[move]}")
+
+if move <=2:
+    print(f"Computer: {rps[random_int]} ||",f"You: {rps[move]}")
+
 if random_int == move:
     print("tie game!")
 elif move == random_int + 1:
-    print("Player Wins!")
-
+    print("You Win!")
 elif random_int == move + 1:
     print("Computer Wins!")
 elif random_int - 2 == move:
-    print("Player Wins!")
+    print("You Win!")
 elif move - 2 == random_int:
     print("Computer Wins!")
 elif move > 2:
