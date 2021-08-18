bf4fedb49ad0c913d4eb112669031af525821637
grant-create <grant.s.wilkie@gmail.com>
2021-08-18 14:52:44 -0700

    day one done

---
+++ day1/band_nam_gen.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+hometown = input("Type the name of the town you grew up in")
+petname = input("type the name of a pet/animal")
+print(f"your band name is {hometown} {petname}")
