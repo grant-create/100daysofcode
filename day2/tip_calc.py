336c29965368472d86a3d884d99acb83c8ef5019
grant-create <grant.s.wilkie@gmail.com>
2021-08-18 15:14:07 -0700

    day2

--- day2/tip_calc.py
+++ day2/tip_calc.py
@@ -1,8 +1,8 @@
 print("Welcome to the tip calculator")
-total = int(input("What was the total bill? "))
+total = float(input("What was the total bill? "))
 people = int(input("How many people are splitting the bill? "))
 tip = int(input("What percentage tip would you like to leave? 10, 12, 15, 20? "))
 
-pay = (total + (total*(tip/100)))/people
+pay = round((total + (total*(tip/100)))/people,2)
 
 print(f"Each person should pay: {pay}")
