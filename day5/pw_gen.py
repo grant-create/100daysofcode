6bec61fd82824c3a94731c174d8ca9480b6763f5
grant-create <grant.s.wilkie@gmail.com>
2021-08-19 14:23:05 -0700

    day6

--- day5/pw_gen.py
+++ day5/pw_gen.py
@@ -1,18 +1,15 @@
 import random
 chars = [
- ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
 ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
- ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+['!', '#', '$', '%', '&', '(', ')', '*', '+']
 ]
 print("Welcome to the PyPassword Generator!")
 letters= int(input("How many letters would you like in your password?\n")) 
 symbols = int(input(f"How many symbols would you like?\n"))
 numbers = int(input(f"How many numbers would you like?\n"))
-
 listofoptions = [letters, symbols, numbers]
 names = ["letters", "symbols", "numbers"]
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
@@ -44,7 +41,7 @@ for x in range(0,total_len):
 # Join the list together
 secure_pw = ["".join(pw)]
         
-print(count, len(pw))
+# print(count, len(pw))
 
 # The secure password!!!
 print(secure_pw[0])
