0252a24cd67564c2f05020603003b290fc2f0e2f
grant-create <grant.s.wilkie@gmail.com>
2021-08-19 15:09:17 -0700

    maze problem

--- day6/maze.py
+++ day6/maze.py
@@ -1,1 +1,37 @@
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+    
+# def jump():
+#     turn_left()
+#     while not right_is_clear():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while not wall_in_front():
+#         move()
+    
+    
+#     turn_left()
+    
+# while at_goal() == False:
+
+    
+#     if wall_in_front() == False and right_is_clear() ==False:
+#         move()
+    
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front() and wall_on_right():
+#         turn_left()
+   
+
+# print(at_goal())
