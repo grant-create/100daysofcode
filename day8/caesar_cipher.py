541bdc3c35116471deecd1e2110fbdcd7c52e4b6
grant-create <grant.s.wilkie@gmail.com>
2021-08-20 12:45:11 -0700

    fixes to day8

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -7,19 +7,26 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caesar(direction,text, shift):
+    encoded_word = ""
     if direction == 'encode':
-        encoded_word = ""
         for letter in text:
-            if alphabet.index(letter) + shift > len(alphabet):
+            
+            if letter not in alphabet:
+                encoded_word += letter
+            
+            elif alphabet.index(letter) + shift > len(alphabet):
                 encoded_word += alphabet[(alphabet.index(letter) + shift)-26]
+            
             else:
                 encoded_word += alphabet[alphabet.index(letter) + shift]
         print(encoded_word)
     elif direction == 'decode':
-        decoded_word = ""
-    for letter in text:
-        
-        decoded_word += alphabet[alphabet.index(letter) - shift]
+        encoded_word = ""
+        for letter in text:
+            if letter not in alphabet:
+                encoded_word += letter
+            else:
+                encoded_word += alphabet[alphabet.index(letter) - shift]
 
-    print(decoded_word)
+    print(encoded_word)
 caesar(direction, text, shift)
