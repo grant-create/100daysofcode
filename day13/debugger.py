0c7f88c83784e6906dbcba1b3ff86d4973455498
grant-create <grant.s.wilkie@gmail.com>
2021-08-29 19:53:07 -0700

    didnt save

--- day13/debugger.py
+++ day13/debugger.py
@@ -1,6 +1,20 @@
-def my_funtction():
-    for i in range(1,21):
-        
-        if i == 20:
-            print('you got it')
-my_funtction()
+# debugging tips
+"""
+use print, use pythontutor.com to visualize code
+take a break
+
+
+
+
+
+1. describe the problem
+2. reproduce the bug
+3. play computer
+4. fix the errors
+5. use print statements
+6. use a debugger if all else fails
+7. take a break
+8. ask a friend
+9. run the code often to get bugs out early and often
+10. stack overflow
+"""
