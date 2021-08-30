f23d5bd14e88e338f6c387162082de54177d2f79
grant-create <grant.s.wilkie@gmail.com>
2021-08-30 09:42:02 -0700

    starting day 15

---
+++ day15/coffee.py
@@ -0,0 +1,12 @@
+from machineinfo import MENU
+"""
+What would you like? (espresso/latte/capuccino): 
+or 
+report - tells how much of each ingredient we have (including money)
+or 
+off
+
+need prices for each drink and be able to calculate change from money inserted
+
+remove ingredients after each transaction
+"""
