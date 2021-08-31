4c6d7c72ed36f35c163b17b0ffeee048f9f5d4d8
grant-create <grant.s.wilkie@gmail.com>
2021-08-31 13:06:19 -0700

    update

--- day15/coffee.py
+++ day15/coffee.py
@@ -11,6 +11,11 @@ need prices for each drink and be able to calculate change from money inserted
 
 remove ingredients after each transaction
 """
+quarter = .25
+dime = .1
+nickel = .05
+penny =.01
+
 
 water = resources["water"]
 milk = resources["milk"]
@@ -24,20 +29,30 @@ def get_coffee():
     while order != 'off':
         if order in MENU:
             print(f"The {order} costs:",MENU[order]["cost"])
-            if MENU[order]["ingredients"]["water"] >= water:
+
+            if MENU[order]["ingredients"]["water"] <= water:
                 water -=  MENU[order]["ingredients"]["water"]
             else:
                 print("Not enough water")
                 
-            if MENU[order]["ingredients"]["milk"] >= milk:
+            if MENU[order]["ingredients"]["milk"] <= milk:
                 milk -=   MENU[order]["ingredients"]["milk"]
             else:
                 print("Not enough milk")
                 
-            if MENU[order]["ingredients"]["coffee"] >= coffee:
+            if MENU[order]["ingredients"]["coffee"] <= coffee:
                 coffee -=  MENU[order]["ingredients"]["coffee"]
             else:
                 print("Not enough coffee")
+            if (MENU[order]["ingredients"]["water"] <= water) and (MENU[order]["ingredients"]["milk"] <= milk) and (MENU[order]["ingredients"]["coffee"] <= coffee):
+                print(f'That will cost: {MENU[order]["cost"]}')
+                print("How will you pay for that?")
+                quarters = int(input("Quarters: ")) * quarters
+                dimes = int(input("Dimes: ")) * dime
+                nickels = int(input("Nickels: ")) * nickel
+                pennies = int(input("Pennies: ")) * penny
+                
+                user_pay = quarters + dimes + nickels + pennies
                 
         
             
