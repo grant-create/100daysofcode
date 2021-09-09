907513643935a11a1b528ad85853471599633d4a
grant-create <grant.s.wilkie@gmail.com>
2021-09-09 16:36:08 -0700

    still going

--- day17/quiz_game.py
+++ day17/quiz_game.py
@@ -2,9 +2,15 @@ class User:
     def __init__(self, name, id):
         self.name = name
         self.id = id
+        self.hands = 2
+        self.followers = 0
+        self.folling = 0
+    def become_pirate(self):
+        self.hands = 1
 
-        print("new user")
-        print(name, id)
+    def follow(self, user):
+        user.followers +=1
+        self.following +=1
 
 
 user_1 = User("jack" , 1)
