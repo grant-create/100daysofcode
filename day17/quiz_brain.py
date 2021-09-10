334d3db0ab6a1d966974c0fa09f49cff5f1fb2ee
grant-create <grant.s.wilkie@gmail.com>
2021-09-10 13:19:34 -0700

    added score

--- day17/quiz_brain.py
+++ day17/quiz_brain.py
@@ -2,7 +2,7 @@ class Quiz_Brain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
-
+        self.score = 0
     def still_has_questions(self):
         if self.question_number < len(self.question_list):
             return True
@@ -15,4 +15,18 @@ class Quiz_Brain:
         current_question = self.question_list[self.question_number]
         self.question_number +=1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False) ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer[0].lower() ==  correct_answer.lower():
+            print("")
+            print("Correct")
+            self.score +=1
+        else:
+            print("")
+            print("Wrong")
+        print(f"The correct answer was {correct_answer}")
+        print(f"Your current score is {self.score}/{self.question_number}")
+        print("")
+
         
